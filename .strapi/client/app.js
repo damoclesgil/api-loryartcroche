c21b2c4816a10244ea1ee8ff5a3bdb2abac8c109
damoclesgil <damoclesgil@gmail.com>
2024-05-29 14:46:04 -0300

    Feat: add mudou nada

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -3,10 +3,10 @@
  * Any modifications made will be discarded.
  */
 import strapiCloud from "@strapi/plugin-cloud/strapi-admin";
+import colorPicker from "@strapi/plugin-color-picker/strapi-admin";
 import email from "@strapi/plugin-email/strapi-admin";
 import graphql from "@strapi/plugin-graphql/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
-import colorPicker from "@strapi/plugin-color-picker/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
 import customisations from "../../src/admin/app.tsx";
@@ -16,9 +16,9 @@ renderAdmin(document.getElementById("strapi"), {
 
   plugins: {
     "strapi-cloud": strapiCloud,
+    "color-picker": colorPicker,
     email: email,
     graphql: graphql,
     "users-permissions": usersPermissions,
-    "color-picker": colorPicker,
   },
 });
